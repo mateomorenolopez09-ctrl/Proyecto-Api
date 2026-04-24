a95f5fce2d8fa14c603d8e0c81efc565a5ea1688
SergiGom <lol10100020@gmail.com>
2026-04-24 17:41:26 -0500

    todo cool

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@
         const app = require('./server');
         const cors = require('cors');
         const port = process.env.PORT || 3000;
-        const host = process.env.HOST || '10.1.196.156';
+        const host = process.env.HOST || '192.168.137.1';
         
         // Configuración CORS
         app.use(cors({
           origin: [
-                'http://10.1.196.156',
+                'http://192.168.137.1',
                 'http://localhost', 
                 'http://127.0.0.1'    
               ],
